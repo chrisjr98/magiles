1d5007fee75c1e8e48c1b6399844cd92e8b3bfb4
chris <christian.alejandro98@hotmail.com>
2020-07-19 20:30:08 -0500

    mi primer commit

---
+++ sample-git/src/test/java/ec/edu/epn/git/PersonaTest.java
@@ -0,0 +1,34 @@
+package ec.edu.epn.git;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonaTest {
+
+	@Test
+	public void testGetAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPersona() {
+		fail("Not yet implemented");
+	}
+
+}
